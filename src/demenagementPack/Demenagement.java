687669f5bc5b4c363f1ae3cb7a62834d16b5ab00
Clowiser <jessica.rigaud@le-campus-numerique.fr>
2021-11-04 15:58:02 +0100

    Exo algorithmique du déménagement

--- src/demenagementPack/Demenagement.java
+++ src/demenagementPack/Demenagement.java
@@ -3,12 +3,49 @@ package demenagementPack ;
 public class Demenagement {
 	private String nom; // attributs
 	private int capCamion;
-	public Demenagement(String nom,int capCamion) { // constructeur
+	
+
+	public Demenagement(String nom,int capCamion) { // constructeur/methode
 		this.nom = nom;
 		this.capCamion = capCamion;
 		}
-	public void auTravail(){
+	
+
+	public void auTravail(int i){
 		System.out.println(nom + " possède un camion de capacité : " + capCamion);
 		System.out.println("Début du déménagement");
+		//System.out.println("nbCartons:" + i + " capCamion:" + capCamion);
+		
+		int voyage = 0;
+		
+		
+		while (i > 0){
+			voyage++;
+			if (i <= 9) {
+				System.out.println("emport :" + i);
+			}else{
+				System.out.println("emport :" + capCamion);
+			}
+			System.out.println("nbCartons:" + i + " capCamion:" + capCamion + " nbvoyage:" + voyage);
+			i -= capCamion; // on commence le calcul de soustraction
+			
+			
+			
+			//if (i < capCamion){ // quand le calcul est avant l'affichage 
+			//break;
+			// affichait -2 donc il fallait stopper la boucle = break
+		//}
 		}
+		
+		System.out.println("Fin du déménagement");
+		
+		
+	};
+	
+	
+	// 34 - capCamion = nbCartons à chaque voyage
+	// emport = nbCartons à chaque voyage
+	// tant que nbCartons <=9 alors on refait un voyage
+	
+	
 }
--- src/demenagementPack/TestDem.java
+++ src/demenagementPack/TestDem.java
@@ -1,11 +1,13 @@
 package demenagementPack ;
 
 public class TestDem{
-	public static void main(String[] args) {
+	public static void main(String[] args) { // String = classe
 		System.out.println("Bienvenue");
+		
 		// on créé une 'instance'
 		Demenagement dem = new Demenagement("Entreprise bretonne",9);
+		
 		// on apelle la méthode d'instance 'auTravail()'
-		dem.auTravail();
+		dem.auTravail(34);
 	}
 }
